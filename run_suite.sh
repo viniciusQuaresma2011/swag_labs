5bbf8736a48858f05e678f29ea747fea2559331f
viniciusQuaresma2011 <85623269+viniciusQuaresma2011@users.noreply.github.com>
2023-07-17 13:00:19 -0300

    Update and rename docker-compose.yml to run_suite.sh

---
+++ run_suite.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+# Iniciar o servidor Xvfb
+Xvfb :99 -screen 0 1280x1024x24 &
+export DISPLAY=:99
+
+sleep 5
+
+# Executar os testes do Robot Framework usando o Firefox
+robot --variable BROWSER:firefox /app/tests/resources/data/Carrinho/Carrinho.robot
